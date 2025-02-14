02b8927afd5f942eb97b8496be3af74b34bc097e
Tresor Dan Minor <93678681+Larchitect3@users.noreply.github.com>
2025-02-14 01:07:12 -0500

    Add files via upload

---
+++ Git notes.c/Notes.c
@@ -0,0 +1,36 @@
+// Winter 2025
+// brew install git : to install git if you have homebrew installed
+//
+//
+// touch "filename.ext" -->creates a file
+// git clone "url" --> gets you a copy of the repository or the URL you want
+// git add -A --> adds all the files
+// git add "filename.ext" -->
+// git commit -m "Message"
+// git push -> publishes your local commits
+// Git status -> checks the status and tells you what commands to run next.
+//
+// git commit -am "message" --> commits all the messages at the same time.
+//
+// git pull "URL" -->gets you the most updated (latest) version of the URL whether it is code or simple text
+//
+//
+//
+// editing the user process
+// If the user is presented with a vi editor, in that case the user will have to :
+
+// Go into insert mode by hitting i key, you should see-- INSERT-- displayed at the bottom of the screen Make changes to the contents Once done, you need to save your changes and exit the editor.For this go into command mode by hitting Esc key; you should see the -- INSERT -- word has been cleared. Now type :wq (stands for write and quit) and hit return key(Enter key). Your config changes ahve been saved.
+// To verify you changes type git config --list or git config --list --global
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
